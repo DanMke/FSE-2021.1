82bacfe3c806dbeb1a46ff210a9c862ba64cff5a
Daniel Maike <danmke@hotmail.com>
2021-09-03 16:26:31 -0300

    Refactor modbus code and get TE TR
    Signed-off-by: Daniel Maike <danmke@hotmail.com>

--- projeto-1/src/main.c
+++ projeto-1/src/main.c
@@ -44,7 +44,7 @@
 #include "i2clcd.h"
 #include "crc16.h"
 
-void showInLCD(int fd, float externalTemperature, float referenceTemperature, float internalTemperature);
+void show_in_lcd(int fd, float externalTemperature, float referenceTemperature, float internalTemperature);
 
 /*!
  * @brief Function reads temperature, humidity and pressure data in forced mode.
@@ -63,7 +63,19 @@ int8_t stream_sensor_data_forced_mode(struct bme280_dev *dev);
 
 int8_t get_data_from_bme280(struct bme280_dev *dev, struct bme280_data *comp_data);
 
-void loop(struct bme280_dev dev, int fd);
+int initialize_uart();
+
+void write_in_uart(int uart0_filestream, char device_code, char code, char subcode);
+
+void read_data_uart(int uart0_filestream, unsigned char *rx_buffer);
+
+float request_internal_temperature(int uart0_filestream);
+
+float request_potentiometer_temperature(int uart0_filestream);
+
+int request_key_state(int uart0_filestream);
+
+void loop(struct bme280_dev dev, int fd, int uart0_filestream);
 
 /*!
  * @brief This function starts execution of the program.
@@ -128,94 +140,23 @@ int main(int argc, char* argv[]) {
 
     // ----------   modbus -----------------
 
-    int uart0_filestream = -1;
+    int uart0_filestream = initialize_uart();
 
-    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);      //Open in non blocking read/write mode
-    if (uart0_filestream == -1) {
-        printf("Erro - Não foi possível iniciar a UART.\n");
-    } else {
-        printf("UART inicializada!\n");
-    }
-    struct termios options;
-    tcgetattr(uart0_filestream, &options);
-    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;     //<Set baud rate
-    options.c_iflag = IGNPAR;
-    options.c_oflag = 0;
-    options.c_lflag = 0;
-    tcflush(uart0_filestream, TCIFLUSH);
-    tcsetattr(uart0_filestream, TCSANOW, &options);
-
-    unsigned char tx_buffer[9];
-
-    tx_buffer[0] = 0x01;
-    tx_buffer[1] = 0x23;
-    tx_buffer[2] = 0xC1;
-
-    char university_registration[4] = {7, 0, 0, 3};
-
-    memcpy(&tx_buffer[3], &university_registration, 4);
-
-    short crc = calcula_CRC(tx_buffer, 7);
-
-    memcpy(&tx_buffer[7], &crc, 2);
-
-    printf("Buffers de memória criados!\n");
-
-    if (uart0_filestream != -1) {
-        printf("Escrevendo caracteres na UART ...");
-
-        int count = write(uart0_filestream, tx_buffer, 9);
-        if (count < 0) {
-            printf("UART TX error\n");
-        } else {
-            printf("escrito.\n");
-        }
-    }
-
-    sleep(2);
 
     //----- CHECK FOR ANY RX BYTES -----
-    if (uart0_filestream != -1) {
-        // Read up to 255 characters from the port if they are there
-        unsigned char rx_buffer[9];
-        int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);      //Filestream, buffer to store in, number of bytes to read (max)
-        if (rx_length < 0) {
-            printf("Erro na leitura.\n"); //An error occured (will occur if there are no bytes)
-        } else if (rx_length == 0) {
-            printf("Nenhum dado disponível.\n"); //No data waiting
-        } else {
-            short crc = calcula_CRC(rx_buffer, rx_length - 2);
-            short oldCrc;
-            memcpy(&oldCrc, rx_buffer + (rx_length - 2), 2);
 
-            if (crc != oldCrc) {
-                close(uart0_filestream);
-                exit(1);
-            }
 
-            //Bytes received
-            rx_buffer[rx_length] = '\0';
-//            int currentInteger;
-//            memcpy(&currentInteger, rx_buffer, rx_length);
-//            printf("INT %d: %d\n", rx_length, currentInteger);
-//            printf("%i Bytes lidos : %d\n", rx_length, currentInteger);
-            float currentFloat;
-            memcpy(&currentFloat, rx_buffer+3, 4);
-            printf("FLOAT %d: %f\n", rx_length, currentFloat);
-            printf("%i Bytes lidos : %f\n", rx_length, currentFloat);
-        }
-    }
-
-    close(uart0_filestream);
 
     // ----------   loop -----------------
 
-    loop(dev, fd);
+    loop(dev, fd, uart0_filestream);
+
+    close(uart0_filestream);
 
     return 0;
 }
 
-void showInLCD(int fd, float externalTemperature, float referenceTemperature, float internalTemperature) {
+void show_in_lcd(int fd, float externalTemperature, float referenceTemperature, float internalTemperature) {
     ClrLcd(fd);
 
     lcdLoc(LINE1, fd);
@@ -295,7 +236,136 @@ int8_t get_data_from_bme280(struct bme280_dev *dev, struct bme280_data *comp_dat
     return rslt;
 }
 
-void loop(struct bme280_dev dev, int fd) {
+int initialize_uart() {
+    int uart0_filestream = -1;
+
+    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);      //Open in non blocking read/write mode
+    if (uart0_filestream == -1) {
+        printf("Erro - Não foi possível iniciar a UART.\n");
+        exit(1);
+    } else {
+        printf("UART inicializada!\n");
+    }
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;     //<Set baud rate
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+
+    return uart0_filestream;
+}
+
+void write_in_uart(int uart0_filestream, char device_code, char code, char subcode) {
+    unsigned char tx_buffer[9];
+
+    tx_buffer[0] = device_code;
+    tx_buffer[1] = code;
+    tx_buffer[2] = subcode;
+
+    char university_registration[4] = {7, 0, 0, 3};
+
+    memcpy(&tx_buffer[3], &university_registration, 4);
+
+    short crc = calcula_CRC(tx_buffer, 7);
+
+    memcpy(&tx_buffer[7], &crc, 2);
+
+    printf("Buffers de memória criados!\n");
+
+    if (uart0_filestream != -1) {
+        printf("Escrevendo caracteres na UART ...");
+
+        int count = write(uart0_filestream, tx_buffer, 9);
+        if (count < 0) {
+            printf("UART TX error\n");
+        } else {
+            printf("escrito.\n");
+        }
+    }
+}
+
+void read_data_uart(int uart0_filestream, unsigned char *rx_buffer) {
+    if (uart0_filestream == -1) {
+        exit(1);
+    }
+    // Read up to 255 characters from the port if they are there
+    int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);      //Filestream, buffer to store in, number of bytes to read (max)
+    if (rx_length < 0) {
+        printf("Erro na leitura.\n"); //An error occured (will occur if there are no bytes)
+        exit(1);
+    } else if (rx_length == 0) {
+        printf("Nenhum dado disponível.\n"); //No data waiting
+        exit(1);
+    } else {
+        short crc = calcula_CRC(rx_buffer, rx_length - 2);
+        short oldCrc;
+        memcpy(&oldCrc, rx_buffer + (rx_length - 2), 2);
+
+        if (crc != oldCrc) {
+            close(uart0_filestream);
+            exit(1);
+        }
+
+        //Bytes received
+        rx_buffer[rx_length] = '\0';
+    }
+}
+
+float request_internal_temperature(int uart0_filestream) {
+    write_in_uart(uart0_filestream, 0x01, 0x23, 0xC1);
+
+    sleep(2);
+
+    unsigned char *rx_buffer;
+    rx_buffer = (unsigned char*) malloc(9 * sizeof(unsigned char));
+    read_data_uart(uart0_filestream, rx_buffer);
+
+    float currentFloat;
+    memcpy(&currentFloat, rx_buffer+3, 4);
+
+    free(rx_buffer);
+
+    return currentFloat;
+}
+
+float request_potentiometer_temperature(int uart0_filestream) {
+    write_in_uart(uart0_filestream, 0x01, 0x23, 0xC2);
+
+    sleep(2);
+
+    unsigned char *rx_buffer;
+    rx_buffer = (unsigned char*) malloc(9 * sizeof(unsigned char));
+    read_data_uart(uart0_filestream, rx_buffer);
+
+    float currentFloat;
+    memcpy(&currentFloat, rx_buffer+3, 4);
+
+    free(rx_buffer);
+
+    return currentFloat;
+}
+
+int request_key_state(int uart0_filestream) {
+    write_in_uart(uart0_filestream, 0x01, 0x23, 0xC3);
+
+    sleep(2);
+
+    unsigned char *rx_buffer;
+    rx_buffer = (unsigned char*) malloc(9 * sizeof(unsigned char));
+    read_data_uart(uart0_filestream, rx_buffer);
+
+    int currentInteger;
+    memcpy(&currentInteger, rx_buffer+3, 4);
+
+    free(rx_buffer);
+
+    return currentInteger;
+}
+
+void loop(struct bme280_dev dev, int fd, int uart0_filestream) {
     /* Structure to get the pressure, temperature and humidity values */
     struct bme280_data comp_data;
 
@@ -306,12 +376,17 @@ void loop(struct bme280_dev dev, int fd) {
             exit(1);
         }
 
-        printf("testando %0.2lf deg C\n", comp_data.temperature);
+        float externalTemperature = comp_data.temperature;
+        float referenceTemperature = request_potentiometer_temperature(uart0_filestream);
+        float internalTemperature = request_internal_temperature(uart0_filestream);
+        int keyState = request_key_state(uart0_filestream);
 
-        float externalTemperature = 25.55;
-        float referenceTemperature = 26.66;
-        float internalTemperature = 27.77;
-        showInLCD(fd, externalTemperature, referenceTemperature, internalTemperature);
+        printf("TE %0.2lf\n", externalTemperature);
+        printf("TR %0.2lf\n", referenceTemperature);
+        printf("TI %0.2lf\n", internalTemperature);
+        printf("KEY STATE %d\n", keyState);
+
+        show_in_lcd(fd, externalTemperature, referenceTemperature, internalTemperature);
 
         sleep(2);
     }
